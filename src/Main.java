c750cedc991157410c6a41dad15c2bfc7f14f762
Jose3708k <josenahumacunalopez8@gmail.com>
2024-06-24 20:40:03 -0600

    for each statement

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,10 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world");
+
+        String[] names = {"Jose","Alex","Miguel","Juan","Mari","Carlos","Jenny"};
+        for (String name:names){
+            System.out.println(name);
+        }
     }
 }
